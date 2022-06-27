e91881ca97f0a126aa8ae21aa8b54f614643a12f
rezamuaz <rezamuaz13@gmail.com>
2022-06-27 20:32:00 +0700

    Update home_services.dart
    version

--- lib/modules/home/repositories/services/home_services.dart
+++ lib/modules/home/repositories/services/home_services.dart
@@ -8,7 +8,7 @@ class HomeServices {
   Future<List<RekomendasiModel>> getRecommendation() async {
     try {
       final response = await Dio()
-          .get('https://my-json-server.typicode.com/rezamuaz/kdrama/db');
+          .get('https://my-json-server.typicode.com/rezamuaz/kdramaapi/db');
       if (response.statusCode == 200 && response.data.isNotEmpty) {
         return (response.data['rekomendasi'] as List)
             .map((e) => RekomendasiModel.fromJson(e))
@@ -24,7 +24,7 @@ class HomeServices {
   Future<List<EpisodeTerbaruModel>> getUpdateEpisode() async {
     try {
       Response response = await Dio()
-          .get('https://my-json-server.typicode.com/rezamuaz/kdrama/db');
+          .get('https://my-json-server.typicode.com/rezamuaz/kdramaapi/db');
       if (response.statusCode == 200 && response.data.isNotEmpty) {
         return (response.data['update'] as List)
             .map((e) => EpisodeTerbaruModel.fromJson(e))
@@ -40,7 +40,7 @@ class HomeServices {
   Future<List<CompletedModel>> getCompleted() async {
     try {
       Response response = await Dio()
-          .get('https://my-json-server.typicode.com/rezamuaz/kdrama/db');
+          .get('https://my-json-server.typicode.com/rezamuaz/kdramaapi/db');
       if (response.statusCode == 200 && response.data.isNotEmpty) {
         return (response.data['tamat'] as List)
             .map((e) => CompletedModel.fromJson(e))
